75f812da43b4127ff27b3ec442e704bc28237d39
AllanCastro24 <alan.castro.1226.ac@gmail.com>
2023-01-09 23:33:48 -0700

    backend y carrito de compras terminado

--- python/web/main.py
+++ python/web/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flaskext.mysql import MySQL
+from datetime import datetime
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -40,19 +41,47 @@ def registro():
 #HOME
 @app.route('/home', methods=['GET','POST'])
 def home():
+   # Definir arrays antes de utilizar para que no de error el front
    datos = []
    if request.method == 'GET':
       conn = mysql.connect()
       cursor = conn.cursor()
-      cursor.execute('SELECT ventas.total,resumen_compra.fecha_y_hora, ventas.folio_resumen FROM ventas INNER JOIN resumen_compra ON ventas.c_resumen_compra = resumen_compra.clave')
+      cursor.execute('SELECT ventas.total,resumen_compra.fecha_y_hora, ventas.folio_resumen, productos.nombre FROM ventas INNER JOIN resumen_compra ON ventas.c_resumen_compra = resumen_compra.clave INNER JOIN productos ON resumen_compra.c_producto = productos.clave')
       datos = cursor.fetchall()
 
    return render_template('dashboard.html', datos=datos)
 
 #CARRITO
-@app.route('/carrito')
-def carrito():
-   return render_template('carrito.html')
+@app.route('/carrito/<id>', methods=['GET','POST'])
+def carrito(id):
+   datos = []
+   productos = []
+
+   if id != "0":
+      # print('No entré al 0')
+      conn = mysql.connect()
+      cursor = conn.cursor()
+      cursor.execute("SELECT * FROM resumen_compra INNER JOIN productos ON resumen_compra.c_producto = productos.clave INNER JOIN unidad_medida ON productos.c_unidad_medida = unidad_medida.clave WHERE resumen_compra.folio = "+ id +";")
+      datos = cursor.fetchall() 
+      # print(datos)
+   else:
+      print("Entré al 0")
+
+   conn = mysql.connect()
+   cursor = conn.cursor()
+   cursor.execute("SELECT * FROM productos INNER JOIN unidad_medida ON productos.c_unidad_medida = unidad_medida.clave;")
+   productos = cursor.fetchall() 
+
+   if request.method == 'POST':
+      print("POST")
+      print(request.form)
+      conn = mysql.connect()
+      cursor = conn.cursor()
+      # INSERT INTO canasta_db.resumen_compra (c_producto, cantidad, fecha_y_hora, folio) VALUES(%s, %s, %s, %s);
+      cursor.execute("INSERT INTO canasta_db.resumen_compra (c_producto, cantidad, fecha_y_hora, folio) VALUES(%s, %s, %s, %s);",(request.form['producto'],request.form['cantidad'],datetime.now(),id))
+      conn.commit()
+
+   return render_template('carrito.html', datos=datos, productos = productos)
 
 if __name__ == '__main__':
    app.debug = True
--- python/web/static/master.js
+++ python/web/static/master.js
@@ -7,3 +7,63 @@ function exportCanvas(){
         document.location.href = dato;	
     },false);
 }
+
+(function () {
+    'use strict'
+
+    feather.replace()
+
+    var ctx = document.getElementById('myChart')
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [
+                'Domingo',
+                'Lunes',
+                'Martes',
+                'Miercoles',
+                'Jueves',
+                'Viernes',
+                'Sabado',
+                'Domingo',
+                'Lunes',
+                'Martes',
+                'Miercoles',
+                'Jueves',
+                'Viernes',
+                'Sabado'
+            ],
+            datasets: [{
+                data: [
+                    89.7,
+                    590,
+                    17.9,
+                    44,
+                    239.2,
+                    84,
+                    17.9,
+                    29.9,
+                    104,
+                    149.7
+                ],
+                lineTension: 0,
+                backgroundColor: 'transparent',
+                borderColor: '#007bff',
+                borderWidth: 4,
+                pointBackgroundColor: '#007bff'
+            }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: false
+                        }
+                    }]
+                },
+                legend: {
+                    display: false
+                }
+            }
+        })
+    })()
