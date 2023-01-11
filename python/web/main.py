33b7100d897fd08ae8f8701c75171800678603d8
AllanCastro24 <alan.castro.1226.ac@gmail.com>
2023-01-10 23:28:43 -0700

    open cv complete

--- python/web/main.py
+++ python/web/main.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, Response
 from flaskext.mysql import MySQL
 from datetime import datetime
+import cv2
+import numpy as np
 # from pyswip import Prolog # Conexión con prolog
 
 # prolog = Prolog() #Crear objeto prolog
+cap = cv2.VideoCapture(1)
 app = Flask(__name__)
 mysql = MySQL()
 
@@ -13,6 +16,144 @@ app.config['MYSQL_DATABASE_DB'] = 'canasta_db'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
 mysql.init_app(app)
+
+def gen_frames():  
+    while True:
+        success, frame = cap.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+def capturar():
+   while(1):
+      _, img = cap.read()
+
+      # Convertir el frame (img == BGR) en HSV(hue-saturation-value)
+
+      hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+      # Identificar frutas
+      apple_upper = np.array([78,2,0],np.uint8)
+      apple_lower = np.array([255,27,20],np.uint8)
+      
+      # Color rojo
+
+      red_lower = np.array([136,87,111],np.uint8)
+      red_upper = np.array([180,255,255],np.uint8)
+
+      # Color azul
+
+      blue_lower = np.array([99,115,150],np.uint8)
+      blue_upper = np.array([110,255,255],np.uint8)
+
+      # Color amarillo
+
+      yellow_lower = np.array([22,60,200],np.uint8)
+      yellow_upper = np.array([60,255,255],np.uint8)
+
+      # Color blanco
+
+      white_lower = np.array([0,0,200],np.uint8)
+      white_upper = np.array([180,20,255],np.uint8)
+
+      # Color negro
+
+      black_lower = np.array([0,0,0],np.uint8)
+      black_upper = np.array([180,255,30],np.uint8)
+
+      # Todos los colores
+      red = cv2.inRange(hsv, red_lower, red_upper)
+      blue = cv2.inRange(hsv, blue_lower, blue_upper)
+      yellow = cv2.inRange(hsv, yellow_lower, yellow_upper)
+      white = cv2.inRange(hsv, white_lower, white_upper)
+      black = cv2.inRange(hsv, black_lower, black_upper)
+      # Frutas
+      apple = cv2.inRange(hsv, apple_lower, apple_upper)
+
+
+      kernal = np.ones((5, 5), "uint8")
+
+      apple = cv2.dilate(apple, kernal)
+      res_apple = cv2.bitwise_and(img, img, mask = apple)
+      
+      red = cv2.dilate(red, kernal)
+      res_red = cv2.bitwise_and(img, img, mask = red)
+
+      blue = cv2.dilate(blue, kernal)
+      res_blue = cv2.bitwise_and(img, img, mask = blue)
+
+      yellow = cv2.dilate(yellow, kernal)
+      res_yellow = cv2.bitwise_and(img, img, mask = yellow)
+
+      white = cv2.dilate(white, kernal)
+      res_white = cv2.bitwise_and(img, img, mask = white)
+
+      black = cv2.dilate(black, kernal)
+      res_black = cv2.bitwise_and(img, img, mask = black)
+
+      # Tracking apple
+      contours, hierarchy = cv2.findContours(apple, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+      for pic, contour in enumerate(contours):
+         area = cv2.contourArea(contour)
+         if(area > 300):
+               x, y, w, h = cv2.boundingRect(contour)
+               img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+               cv2.putText(img, "MANZANA: ", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))    
+
+      # Tracking red
+      contours, hierarchy = cv2.findContours(red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+      for pic, contour in enumerate(contours):
+         area = cv2.contourArea(contour)
+         if(area > 300):
+               x, y, w, h = cv2.boundingRect(contour)
+               img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+               cv2.putText(img, "MANZANA: ", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))    
+
+      # Tracking blue
+      contours, hierarchy = cv2.findContours(blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+      for pic, contour in enumerate(contours):
+         area = cv2.contourArea(contour)
+         if(area > 300):
+               x, y, w, h = cv2.boundingRect(contour)
+               img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+               cv2.putText(img, "AZUL: ", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0))
+               
+      # Tracking yellow
+      contours, hierarchy = cv2.findContours(yellow, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+      for pic, contour in enumerate(contours):
+         area = cv2.contourArea(contour)
+         if(area > 300):
+               x, y, w, h = cv2.boundingRect(contour)
+               img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+               cv2.putText(img, "PLATANO: ", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0))
+
+      # Tracking white
+      contours, hierarchy = cv2.findContours(white, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+      for pic, contour in enumerate(contours):
+         area = cv2.contourArea(contour)
+         if(area > 300):
+               x, y, w, h = cv2.boundingRect(contour)
+               img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
+               cv2.putText(img, "BLANCO: ", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255))
+
+      # Tracking black
+      contours, hierarchy = cv2.findContours(black, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+      for pic, contour in enumerate(contours):
+         area = cv2.contourArea(contour)
+         if(area > 300):
+               x, y, w, h = cv2.boundingRect(contour)
+               img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 0), 2)
+               cv2.putText(img, "NEGRO: ", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0))
+
+      (flag, encodedImage) = cv2.imencode(".jpg", img)
+      if not flag:
+            continue
+            yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +bytearray(encodedImage) + b'\r\n')
+
+
 #LOGIN
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -84,6 +225,10 @@ def carrito(id):
       conn.commit()
 
    return render_template('carrito.html', datos=datos, productos = productos)
+
+@app.route("/video_feed")
+def video_feed():
+   return Response(capturar(), mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 if __name__ == '__main__':
    app.debug = True
